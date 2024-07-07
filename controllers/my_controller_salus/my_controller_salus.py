d4e8cc4b8e168029e3277eafff02fd2bf84c70a5
alswl1208 <165457153+alswl1208@users.noreply.github.com>
2024-07-07 23:27:24 +0900

    Added my_controller_salus.py

---
+++ controllers/my_controller_salus/my_controller_salus.py
@@ -0,0 +1,45 @@
+"""my_controller_salus controller."""
+
+# You may need to import some classes of the controller module. Ex:
+#  from controller import Robot, Motor, DistanceSensor
+from controller import Robot
+
+
+# create the Robot instance.
+robot = Robot()
+
+# get the time step of the current world.
+timestep = int(robot.getBasicTimeStep())
+
+
+
+# You should insert a getDevice-like function in order to get the
+# instance of a device of the robot. Something like:
+#  motor = robot.getDevice('motorname')
+#  ds = robot.getDevice('dsname')
+#  ds.enable(timestep)
+
+
+# initialise sensors - 360 camera
+camera_360 = robot.getDevice("360_camera")
+camera_360.enable(timestep)
+
+# initialise sensors - depth camera (in MiR100)
+# camera_depth = robot.getDevice("depth_camera")
+# camera_depth.enable(timestep)
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+while robot.step(timestep) != -1:
+    # Read the sensors:
+    # Enter here functions to read sensor data, like:
+    #  val = ds.getValue()
+
+    # Process sensor data here.
+
+    # Enter here functions to send actuator commands, like:
+    #  motor.setPosition(10.0)
+    pass
+
+# Enter here exit cleanup code.
+ 
